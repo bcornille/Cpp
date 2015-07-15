8f74e7fd7aa8a75076678b9d89d2295f1099fd8d
bcornille <bcornille7@gmail.com>
2015-07-15 16:23:15 -0500

    Completed exercise for section 1.9 on headers.

---
+++ Chapter1/1.9/add.hpp
@@ -0,0 +1,8 @@
+// Use a header guard to prevent multiple declarations.
+#ifndef ADD_HPP
+#define ADD_HPP
+
+// Forward declaration of the contents of the file add.cpp
+int add(int x, int y);
+
+#endif // ADD_HPP
